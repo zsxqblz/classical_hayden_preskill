c00e280f5d13044ae0177db24acdbc1f6d1fc0b8
Frank Zhang <zsxqblz@gmail.com>
2024-04-05 08:54:02 -0400

    bla

--- test.jl
+++ test.jl
@@ -49,25 +49,25 @@ end
 # scanNoisyMeasOneDepth
 let 
     run(`clear`)
-    for i = 1:4
+    for i = 1:1
         nAsites = 1
         nBsites = 100*i
-        nsteps = 350*i
+        nsteps = 50*i
         nstB = 1000
         nmeas_start = 1
         nmeas_end = nBsites
         nmeas_step = 1
-        pertb_start = 0.15
-        pertb_end = 0.3
-        pertb_step = 0.002
-        idx_start = 80
+        pertb_start = 0.0
+        pertb_end = 0.2
+        pertb_step = 0.001
+        idx_start = 0
 
-        S_ave_arr = scanMeasNoisyCAOneDepth(rule30NoisyStep,nAsites,nBsites,nmeas_start,nmeas_end,nmeas_step,pertb_start,pertb_end,pertb_step,nsteps,nstB)
+        S_ave_arr, S_std_arr = scanMeasNoisyCAOneDepth(rule30NoisyStep,nAsites,nBsites,nmeas_start,nmeas_end,nmeas_step,pertb_start,pertb_end,pertb_step,nsteps,nstB)
 
         nmeas_l = floor.(Int,collect(range(nmeas_start,stop=nmeas_end,step=nmeas_step)))
         pertb_l = collect(range(pertb_start,stop=pertb_end,step=pertb_step))
 
-        save2DData(nmeas_l,pertb_l,S_ave_arr,string("data/240323/240323_",idx_start+i))
+        save2DData(nmeas_l,pertb_l,S_ave_arr,S_std_arr,string("data/240405/240405_",idx_start+i))
     end
 end
 
